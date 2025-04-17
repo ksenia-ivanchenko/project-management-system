e16ed2322c6f51f940828f3f34f708c42d79a85c
Ksenia Ivanchenko <k.ivanchenko20@gmail.com>
2025-04-17 15:14:57 +0700

    MAIN-4 добавлена кнопка создания задачи в хедер

--- client/src/app/layout/main-layout.tsx
+++ client/src/app/layout/main-layout.tsx
@@ -1,11 +1,12 @@
 import { Layout } from 'antd';
 import { Header } from '@widgets/header';
+import styles from './main-layout.module.scss';
 
 const { Content } = Layout;
 
 export const MainLayout = ({ children }) => {
   return (
-    <Layout>
+    <Layout className={styles.layout}>
       <Header />
       <Content>{children}</Content>
     </Layout>
--- client/src/widgets/header/ui/header.tsx
+++ client/src/widgets/header/ui/header.tsx
@@ -1,27 +1,34 @@
-import { Menu } from 'antd';
+import { Button, Menu } from 'antd';
 import { Link, useLocation } from 'react-router-dom';
 import { routes } from '@shared';
+import styles from './header.module.scss';
 
 export const Header = () => {
   const location = useLocation();
 
   const items = [
     {
-      key: routes.boards.pathname,
+      key: routes.boards.getLink(),
       label: <Link to={routes.boards.getLink()}>{routes.boards.pathname}</Link>,
     },
     {
-      key: routes.issues.pathname,
+      key: routes.issues.getLink(),
       label: <Link to={routes.issues.getLink()}>{routes.issues.pathname}</Link>,
     },
   ];
 
   return (
-    <Menu
-      theme="light"
-      mode="horizontal"
-      selectedKeys={[location.pathname]}
-      items={items}
-    />
+    <div className={styles.header}>
+      <Menu
+        theme="light"
+        mode="horizontal"
+        selectedKeys={[location.pathname]}
+        items={items}
+        className={styles.menu}
+      />
+      <Button type="primary" className={styles.button}>
+        Создать задачу
+      </Button>
+    </div>
   );
 };
